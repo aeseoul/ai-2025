509b36c7d4b829f90a44f976d0c9c8014135771d
aeseoul <aeseoul@pukyong.ac.kr>
2025-05-07 13:45:50 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,4 @@
+streamlit_app.py
 import streamlit as st
 from openai import OpenAI
 
@@ -15,5 +16,8 @@ if st.button("Ask!", disabled=(len(prompt)==0)):
     )
 
     st.write(response.output_text)
+===========
+## requirement.txt
+openai
 ## requirement.txt
 openai
